68ef3e8edd45035e41fcc0aff02fbea6830c7283
Errol01 <100408359+Errol01@users.noreply.github.com>
2026-07-16 17:05:42 +0700

    chore: Add IPC API bridge for secure preload communication

---
+++ electron/preload.js
@@ -0,0 +1,61 @@
+/**
+ * IPC API - Preload script bridge for secure IPC communication
+ * Exposes safe API to renderer process
+ */
+
+import { contextBridge, ipcRenderer } from 'electron';
+
+const api = {
+  // Database APIs
+  db: {
+    initialize: () => ipcRenderer.invoke('db:initialize'),
+    close: () => ipcRenderer.invoke('db:close'),
+  },
+
+  // Wallet APIs
+  wallet: {
+    generateMnemonic: () => ipcRenderer.invoke('wallet:generateMnemonic'),
+    validateMnemonic: (mnemonic) => ipcRenderer.invoke('wallet:validateMnemonic', mnemonic),
+    saveMnemonic: (mnemonic) => ipcRenderer.invoke('wallet:saveMnemonic', mnemonic),
+    generateWallets: (count) => ipcRenderer.invoke('wallet:generateWallets', count),
+    getAllWallets: () => ipcRenderer.invoke('wallet:getAllWallets'),
+    updateWallet: (id, updates) => ipcRenderer.invoke('wallet:updateWallet', id, updates),
+    deleteWallet: (id) => ipcRenderer.invoke('wallet:deleteWallet', id),
+    importFromPrivateKey: (privateKey, name) => 
+      ipcRenderer.invoke('wallet:importFromPrivateKey', privateKey, name),
+  },
+
+  // Blockchain APIs
+  blockchain: {
+    initialize: () => ipcRenderer.invoke('blockchain:initialize'),
+    getBalance: (address) => ipcRenderer.invoke('blockchain:getBalance', address),
+    getNonce: (address) => ipcRenderer.invoke('blockchain:getNonce', address),
+    getGasPrice: () => ipcRenderer.invoke('blockchain:getGasPrice'),
+    estimateGas: (from, to, amount) => ipcRenderer.invoke('blockchain:estimateGas', from, to, amount),
+    getChainInfo: () => ipcRenderer.invoke('blockchain:getChainInfo'),
+    validateBatchTransactions: (transactions) => 
+      ipcRenderer.invoke('blockchain:validateBatchTransactions', transactions),
+  },
+
+  // Transaction APIs
+  transaction: {
+    addToQueue: (txData) => ipcRenderer.invoke('transaction:addToQueue', txData),
+    getQueueStatus: () => ipcRenderer.invoke('transaction:getQueueStatus'),
+    getQueue: () => ipcRenderer.invoke('transaction:getQueue'),
+    pauseQueue: () => ipcRenderer.invoke('transaction:pauseQueue'),
+    resumeQueue: () => ipcRenderer.invoke('transaction:resumeQueue'),
+    cancelTransaction: (txId) => ipcRenderer.invoke('transaction:cancelTransaction', txId),
+    clearQueue: () => ipcRenderer.invoke('transaction:clearQueue'),
+  },
+
+  // RPC APIs
+  rpc: {
+    addEndpoint: (rpcData) => ipcRenderer.invoke('rpc:addEndpoint', rpcData),
+    getAllEndpoints: () => ipcRenderer.invoke('rpc:getAllEndpoints'),
+    getPrimaryEndpoint: () => ipcRenderer.invoke('rpc:getPrimaryEndpoint'),
+    deleteEndpoint: (id) => ipcRenderer.invoke('rpc:deleteEndpoint', id),
+    switchRpc: (rpcUrl) => ipcRenderer.invoke('rpc:switchRpc', rpcUrl),
+  },
+};
+
+contextBridge.exposeInMainWorld('electronAPI', api);
